37849611585195a875f3f10221f730939b7b3b49
Ngô Việt Hoàng <162393499+Ngo-Viet-Hoang1@users.noreply.github.com>
2024-11-20 10:26:33 +0700

    feat: using table ag-grid

--- tnine.Web.Host/app/admin/invoice/invoice.component.js
+++ tnine.Web.Host/app/admin/invoice/invoice.component.js
@@ -6,55 +6,78 @@
             controllerAs: 'vm'
         });
 
-    invoiceController.$inject = ['serviceProxies', '$state', '$element'];
+    invoiceController.$inject = ['serviceProxies', '$state', '$element', '$scope', '$timeout'];
 
-    function invoiceController(serviceProxies, $state, $element) {
+    function invoiceController(serviceProxies, $state, $element, $scope, $timeout) {
         var vm = this;
         vm.invoices = [];
         vm.selectedId = null;
 
-        vm.columns = [
-            { headerName: "Id", field: "Id" },
-            { headerName: "CreationTime", field: "CreationTime" },
-            { headerName: "CustomerName", field: "CustomerName" },
-            { headerName: "CustomerTelephone", field: "CustomerTelephone" },
-            { headerName: "PaymentStatusName", field: "PaymentStatusName" },
-            { headerName: "PaymentMethodName", field: "PaymentMethodName" },
-            { headerName: "Total", field: "Total" }
-        ];
-
-        vm.getInvoices = function () {
-            serviceProxies.invoiceService.getAll().then(function (response) {
-                vm.invoices = response;
-            }).catch(function (error) {
-                console.error('Error fetching invoices:', error);
-            });
-        };
-
-        vm.reload = function () {
-            vm.getInvoices();
-        };
-
-        vm.add = function () {
-            angular.element(document.querySelector('create-or-edit-invoice-modal')).controller('createOrEditInvoiceModal').show();
-        };
-
-        vm.edit = function () {
-            angular.element(document.querySelector('create-or-edit-invoice-modal')).controller('createOrEditInvoiceModal').show(vm.selectedId);
-        };
-
-        vm.delete = function () {
-            if (confirm('Are you sure you want to delete?')) {
-                serviceProxies.invoiceService.delete(vm.selectedId).then(function () {
-                    vm.getInvoices();
-                }).catch(function (error) {
-                    console.error('Error deleting invoice:', error);
-                });
+        vm.gridOptions = {
+            columnDefs: [
+                { headerName: "No", valueGetter: "node.rowIndex + 1" },
+                { headerName: "Id", field: "Id" },
+                { headerName: "CreationTime", field: "CreationTime" },
+                { headerName: "CustomerName", field: "CustomerName" },
+                { headerName: "CustomerTelephone", field: "CustomerTelephone" },
+                { headerName: "PaymentStatusName", field: "PaymentStatusName" },
+                { headerName: "PaymentMethodName", field: "PaymentMethodName" },
+                { headerName: "Total", field: "Total", valueGetter: `data.Total.toLocaleString() + ' VNĐ'` },
+            ],
+            rowData: [],
+            pagination: true,
+            defaultColDef: {
+                sortable: true,
+                filter: true,
+                resizable: true,
+                floatingFilter: true,
+                flex: 1,
+                cellStyle: { 
+                    display: 'flex', 
+                    alignItems: 'center', 
+                } 
+            },
+            rowSelection: 'single',
+            enableColResize: true,
+            rowHeight: 35,
+            frameworkComponents: {
+            },
+            onGridReady: function (params) {
+                vm.gridApi = params.api
+                params.api.sizeColumnsToFit()
+            },
+            onFirstDataRendered: function (params) {
+                params.api.sizeColumnsToFit();
+            },
+            onRowClicked: function (event) {
+                $timeout(() => vm.selectedId = event.data.Id)
             }
         };
 
-        vm.handleRowClick = function (row) {
-            vm.selectedId = row.Id;
+        vm.getInvoices = () => 
+            serviceProxies.invoiceService.getAll()
+                .then((response) => {
+                    vm.invoices = response
+                    if (vm.gridApi) {
+                        vm.gridApi.setRowData(vm.invoices);
+                    }
+                })
+                .catch((error) => console.error('Error fetching invoices:', error));    
+
+        vm.reload = () => vm.getInvoices()
+
+        vm.add = () => angular.element(document.querySelector('create-or-edit-invoice-modal'))
+            .controller('createOrEditInvoiceModal').show();
+
+        vm.edit = () => angular.element(document.querySelector('create-or-edit-invoice-modal'))
+            .controller('createOrEditInvoiceModal').show(vm.selectedId);
+
+        vm.delete = () => {
+            if (confirm('Are you sure you want to delete?')) {
+                serviceProxies.invoiceService.delete(vm.selectedId)
+                    .then(() => vm.getInvoices())
+                    .catch((error) => console.error('Error deleting invoice:', error))
+            }
         };
 
         vm.getInvoices();
--- tnine.Web.Host/app/admin/invoice/invoice.module.js
+++ tnine.Web.Host/app/admin/invoice/invoice.module.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-
+    agGrid.initialiseAgGridWithAngular1(angular);
     angular.module('app.admin.invoice', [
         'app.services',
         'app.common'
